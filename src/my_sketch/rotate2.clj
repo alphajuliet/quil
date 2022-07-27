b3c8140faea79a96adab9ec6b57de2b82bb5d65d
alphajuliet <andrew@alphajuliet.com>
2022-07-27 22:16:35 +1000

    Ignore image folders. Add multiple frames rendering.

--- src/my_sketch/rotate2.clj
+++ src/my_sketch/rotate2.clj
@@ -3,14 +3,14 @@
             [quil.middleware :as m]
             [my-sketch.util :as util]))
 
-
 ;;----------------
 (defn setup
   []
-  (q/frame-rate 10) ;fps
+  (q/frame-rate 24) ;fps
   (q/color-mode :hsb)
-  {:shapes (for [i (range 0 500 25)
-                 j (range 0 500 25)]
+  {:render? false
+   :shapes (for [i (range 0 500 50)
+                 j (range 0 500 50)]
              {:x i :y j
               :hue (+ 25 (* 85 (rand-int 3)))
               :brightness 255
@@ -19,7 +19,7 @@
 ;;----------------
 (defn update-shape
   [shape]
-  (if (< (rand-int 30) 1)
+  (if (< (rand-int 48) 1)
     (-> shape
         (update :theta #(mod (+ % (util/d2r 90))
                              (* 2 q/PI)))
@@ -45,16 +45,18 @@
   [state]
   (let [c (q/frame-count)]
     (q/background 30)
-    (q/stroke-weight 3)
+    (q/stroke-weight 8)
     (q/with-translation [12 12]
       (doseq [{:keys [x y hue brightness theta]} (:shapes state)]
         (q/stroke hue 255 brightness)
         (q/with-translation [x y]
-          (tile 0 0 24 theta))))))
+          (tile 0 0 48 theta)
+          (when (:render? state)
+            (q/save-frame "frames/f####.png")))))))
 
 (defn snapshot
   [state _]
-  (q/save-frame "snapshot.png")
+  (q/save-frame "image/snapshot.png")
   state)
 
 ;;----------------
