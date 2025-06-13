eb6cc1747ecc6bca708115d6a6b1d9d56a8584f3
alphajuliet <andrew@alphajuliet.com>
2025-06-13 20:59:11 +1000

    Add deps.edn. Update quil and Clojure versions.

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "http://alphajuliet.com/ns/my-sketch/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
+  :dependencies [[org.clojure/clojure "1.11.3"]
                  [spec-dict "0.2.1"]
-                 [quil "4.3.1323"]
+                 [quil "4.3.1563"]
                  [org.clojure/algo.monads "0.1.6"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}})
